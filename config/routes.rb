aaf508f3806ec6a84105853895e1cc02ec49735d
KanaOyaizu <143867420+kanao0@users.noreply.github.com>
2023-09-17 15:12:52 +0000

    [add]useridcolumn

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root :to => 'homes#top'  
   get 'home/about' => "homes#about"
-  
-  get 'users/edit'
-  get 'books/index'
-  # resources :users, only: [:show, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  # resources :books
+  resources :users, only: [:index, :show, :edit, :update]
   
   
   
