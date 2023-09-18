bcb1d7eda10ea9ab7fd99ffa5fbdf17e60ac083d
KanaOyaizu <143867420+kanao0@users.noreply.github.com>
2023-09-18 15:19:37 +0000

    [finish]header,footer

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,46 @@
 class BooksController < ApplicationController
-  def new
+  # def new
+  # end
+  
+  def index
+    # 新規投稿のデータを入れるための箱を作る名前は@book
+    @book = Book.new
   end
+
+  def create
+    # 新規投稿フォームで入力されたデータが@bookに入る
+    @book = Book.new(book_params)
+    # 投稿したユーザーが誰か
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+  
+  
+  def show
+    # Bookモデルのデータみつけてきて～データの箱は@bookって名前にしたよ
+    @book = Book.find(params[:id])    
+  end
+   
+    private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end
+  
 end
+
+
+# def create
+#     # データを保存するための箱を作る名前は@book
+#     @book = Book.new(book_params)
+
+#     if @book.save
+
+#       redirect_to book_path(@book.id)
+#       flash[:notice] = "You have created book successfully."    
+#     else
+#       @books = Book.all
+#       render :index
+  #   end
+  # end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root :to => 'homes#top'  
   get 'home/about' => "homes#about"
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   # resources :books
   resources :users, only: [:index, :show, :edit, :update]
   
